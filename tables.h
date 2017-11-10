826cd6a2924a5726880402f3814f96e890b5a234
DFXLuna <teamuba@gmail.com>
2017-11-10 12:25:48 -0700

    Checkpoint

--- tables.h
+++ tables.h
@@ -1,11 +1,14 @@
 #include<string>
 using std::string;
+#include<map>
+using std::map;
 
 // It might be worth nesting the classes
 
 class Table;
 class TypeInst;
 class TypeDecl;
+class TypeGen;
 
 
 class TableManager{
@@ -31,15 +34,21 @@ private:
 
 class Table{
 public:
+    Table( Table* parent );
     ~Table();
-    bool tryLookup( string name, TypeDecl* ret );
+    bool tryLookup( string name, TypeGen* ret );
     bool addEntry(/* STUFF NEEDED FOR TypeInst CREATION*/);
 private:
     Table* parent;
+    map<string, TypeGen> table;
 };
 
+// Parent placeholder for possible separate method and var types
+// as well as typedecls for typetable
+class TypeGen{}
+
 // This covers instances of a type
-class TypeInst {
+class TypeInst : public TypeGen {
 public:
     TypeInst( TypeDecl* type, string name );
 private:
@@ -52,7 +61,7 @@ private:
 // This covers the declaration of a type
 // Allows implicit forward declaration so that all types will be in type table
 // before they are actually processed.
-class TypeDecl {
+class TypeDecl : public TypeGen {
     public:
     TypeDecl( string name, int width = 0, forwardDecl = true );
 
